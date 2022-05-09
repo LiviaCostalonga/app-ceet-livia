ccfc7e975f71c3a62ab5b2db8bfeb2cf5460ee00
LiviaCostalonga <liviacj2003@gmail.com>
2022-05-09 09:54:50 -0300

    fazendo o picker

--- telas/Contato.js
+++ telas/Contato.js
@@ -1,6 +1,6 @@
 import react, {useState}  from 'react'; 
 export {StatusBar} from 'expo-status-bar'; 
-import {View,Button,StyleSheet, StatusBar,Image,TextInput,Text,TouchableOpacity} from "react-native";
+import {View,Button,StyleSheet, StatusBar,Image,TextInput,Text,TouchableOpacity,Picker} from "react-native";
 
 
 
@@ -9,12 +9,16 @@ const Contato = () => {
   const [nome,setNome] = useState(''); 
   const [email,setEmail] = useState(''); 
   const [telefone,setTelefone] = useState(''); 
-  const [duvida,setDuvida] = useState(''); 
+  const [texto,setTexto] = useState(''); 
+  const [duvida,setDuvida] = useState('');
+  const [reclamacao,setReclamacao] = useState('');
+  const [sugestao,useSugestao] = useState(''); 
  
   const cadastro = () =>{
     alert(nome);
     alert(email);
-    alert(duvida);
+    alert(telefone); 
+    alert(texto);
   }
 return(
   <View style={styles.container}>
@@ -27,9 +31,15 @@ return(
 
         <TextInput placeholder="Seu e-mail" style={styles.textInput} onChangeText={text=>setEmail(text)} />
 
-        <TextInput placeholder="(99) 99999-9999" style={styles.textInput} onChangeText={text=>setNome(text)} />
+        <TextInput placeholder="(99) 99999-9999" style={styles.textInput} onChangeText={text=>setTelefone(text)} />
 
-        <TextInput placeholder="Sua dúvida, reclamação, sugestão..." style={styles.textInput} onChangeText={text=>setDuvida(text)} />
+        <Picker selectedValue={selectedValue} onValueChange={(itemValue,itemIndex)=> setSelectedValue(itemValue)}>
+          <Picker.Item label="Dúvida" value="duvida" onChangeText={text=>setDuvida(text)}/>
+          <Picker.Item label="Reclamação" value="reclamacao" />
+          <Picker.Item label="Sugestão" value="sugestao" />
+        </Picker>
+
+        <TextInput placeholder="Sua dúvida, reclamação, sugestão..." style={styles.textInput} onChangeText={text=>setTexto(text)} />
 
         <TouchableOpacity style={styles.btnEnviar} onPress={()=>cadastro()}>
           <Text style={{color:'white',textAlign:'center'}}>ENVIAR</Text>
