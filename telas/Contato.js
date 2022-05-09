b89371e87064a09440e4bc4992d4fddd79894628
LiviaCostalonga <liviacj2003@gmail.com>
2022-05-09 04:51:34 -0300

    front da tela de contato

--- telas/Contato.js
+++ telas/Contato.js
@@ -1,25 +1,66 @@
 import react, {useState}  from 'react'; 
-import {View,Text,Button,StyleSheet} from "react-native";
+export {StatusBar} from 'expo-status-bar'; 
+import {View,Button,StyleSheet, StatusBar,Image,TextInput,Text,TouchableOpacity} from "react-native";
 
-const Contador = () =>{
-    const [contador, setContador] = useState(1) // USESTATE É UM HOOK RESPONSAVEL POR MODIFICAR E MONITORAR VARIAVEIS
-                                                //O que sera manipulado é o setContador
-    return(
-      <View style={styles.container}>
-        <Text >
-          {contador}
-        </Text>
-        <Button onPress={()=>{setContador(contador+1)}} title="Me clique" />
-      </View>
-    );
+
+
+const Contato = () => {
+
+  const [nome,setNome] = useState(''); 
+  const [email,setEmail] = useState(''); 
+  const [telefone,setTelefone] = useState(''); 
+  const [duvida,setDuvida] = useState(''); 
+ 
+  const cadastro = () =>{
+    alert(nome);
+    alert(email);
+    alert(duvida);
   }
+return(
+  <View style={styles.container}>
+     <StatusBar hidden/> 
+  
+     <Image style={styles.logo}
+        source={require('../img/logo.png')}/>
 
-  const styles = StyleSheet.create({ 
-    container: { 
-     flex: 1, //pega em toda tela 
-     justifyContent: 'center', //centraliza na horizontal
-     alignItems: 'center' //centraliza na vertical
-    }, 
-  });
+        <TextInput placeholder="Seu nome" style={styles.textInput} onChangeText={text=>setNome(text)} />
 
-  export default Contador; 
+        <TextInput placeholder="Seu e-mail" style={styles.textInput} onChangeText={text=>setEmail(text)} />
+
+        <TextInput placeholder="(99) 99999-9999" style={styles.textInput} onChangeText={text=>setNome(text)} />
+
+        <TextInput placeholder="Sua dúvida, reclamação, sugestão..." style={styles.textInput} onChangeText={text=>setDuvida(text)} />
+
+        <TouchableOpacity style={styles.btnEnviar} onPress={()=>cadastro()}>
+          <Text style={{color:'white',textAlign:'center'}}>ENVIAR</Text>
+        </TouchableOpacity>
+  </View>
+);
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: 20,
+},
+textInput: {
+  width: '100%',
+  height: 40,
+  backgroundColor: '#E6E6E6',
+borderRadius: 20,
+paddingLeft: 10,
+marginBottom: 10
+},
+btnEnviar:{
+width: '100%',
+height: 40,
+borderRadius: 20,
+justifyContent: 'center',
+backgroundColor: '#0431B4'
+},
+});
+
+  export default Contato; 
---
+++ telas/ExUsestate.js
@@ -0,0 +1,25 @@
+import react, {useState}  from 'react'; 
+import {View,Text,Button,StyleSheet} from "react-native";
+
+const ExUsestate = () =>{
+    const [contador, setContador] = useState(1) // USESTATE É UM HOOK RESPONSAVEL POR MODIFICAR E MONITORAR VARIAVEIS
+                                                //O que sera manipulado é o setContador
+    return(
+      <View style={styles.container}>
+        <Text >
+          {contador}
+        </Text>
+        <Button onPress={()=>{setContador(contador+1)}} title="Me clique" />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({ 
+    container: { 
+     flex: 1, //pega em toda tela 
+     justifyContent: 'center', //centraliza na horizontal
+     alignItems: 'center' //centraliza na vertical
+    }, 
+  });
+
+  export default ExUsestate; 
